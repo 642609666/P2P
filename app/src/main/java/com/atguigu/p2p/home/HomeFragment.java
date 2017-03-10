3cadababde1fd6a63987d002e65cdce6cefc61db
642609666 <642609666@qq.com>
2017-03-10 20:50:26 +0800

    增加头部布局

--- app/src/main/java/com/atguigu/p2p/home/HomeFragment.java
+++ app/src/main/java/com/atguigu/p2p/home/HomeFragment.java
@@ -1,11 +1,20 @@
 package com.atguigu.p2p.home;
 
+import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.atguigu.p2p.R;
 import com.atguigu.p2p.base.BaseFragment;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * Created by ${
@@ -16,19 +25,49 @@ import com.atguigu.p2p.base.BaseFragment;
 
 public class HomeFragment extends BaseFragment {
 
-    private TextView mTextView;
+    @InjectView(R.id.base_title)
+    TextView baseTitle;
+    @InjectView(R.id.base_back)
+    ImageView baseBack;
+    @InjectView(R.id.base_setting)
+    ImageView baseSetting;
+
     @Override
     public View initView() {
-        mTextView = new TextView(mContext);
-        mTextView.setText("主页");
-        mTextView.setGravity(Gravity.CENTER);
+        View view = View.inflate(mContext, R.layout.fragment_home, null);
         Log.e("TAG", "主页加载视图成功");
-        return mTextView;
+        return view;
     }
 
     @Override
     protected void initData() {
 
         Log.e("TAG", "主页加载数据成功");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        ButterKnife.inject(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
+
+    @OnClick({R.id.base_back, R.id.base_setting})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.base_back:
+
+                break;
+            case R.id.base_setting:
+                Toast.makeText(mContext, "设置", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
