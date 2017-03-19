14cc78465fc9f9b5838faa42cc13662da067de18
642609666 <642609666@qq.com>
2017-03-19 21:29:41 +0800

    分享项目

--- app/src/main/java/com/atguigu/p2p/WelcomeActivity.java
+++ app/src/main/java/com/atguigu/p2p/WelcomeActivity.java
@@ -1,18 +1,42 @@
 package com.atguigu.p2p;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Environment;
 import android.os.Handler;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.p2p.base.BaseAvtivity;
+import com.atguigu.p2p.bean.UpdateBean;
+import com.atguigu.p2p.utils.AppNetConfig;
+import com.atguigu.p2p.utils.LoadNet;
+import com.atguigu.p2p.utils.ThreadPool;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.InjectView;
 
@@ -39,6 +63,175 @@ public class WelcomeActivity extends BaseAvtivity {
 
         //设置版本号
         setVersions();
+
+        //检查版本是否更新
+        checkUpdate();
+    }
+
+    private void checkUpdate() {
+        if (isOnLine()) {
+            //连网
+            LoadNet.getDataNet(AppNetConfig.UPDATE, new LoadNet.OnGetNet() {
+
+                        @Override
+                        public void onSuccess(String content) {
+                            Log.i("update", "success: " + content);
+                            final UpdateBean updateBean = JSON.parseObject(content, UpdateBean.class);
+                            //判断当前的版本号
+                            if (!getVersions().equals(updateBean.getVersion())) {
+                                //提示有新版本
+                                new AlertDialog.Builder(WelcomeActivity.this)
+                                        .setTitle("有新版本你怎么看？")
+                                        .setMessage(updateBean.getDesc())
+                                        .setPositiveButton("不差钱", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                updateVerison(updateBean.getApkUrl());
+                                            }
+                                        })
+                                        .setNegativeButton("哥很穷", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                toMain();
+                                            }
+                                        }).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(String content) {
+
+                        }
+                    }
+
+            );
+        } else {
+            //不要在动画没有执行完之前做进入主界面的动作
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Toast.makeText(WelcomeActivity.this, "当前没有网络", Toast.LENGTH_SHORT).show();
+                    toMain();
+                }
+            }, 2000);
+        }
+    }
+
+    private void updateVerison(final String apkUrl) {
+        //展示进度条
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.show();
+        //下载
+        ThreadPool.getInstance().getExecutorService().execute(new Runnable() {
+
+            private FileOutputStream os;
+            private InputStream inputStream;
+
+            @Override
+            public void run() {
+                try {
+                    //获取url地址
+                    URL url = new URL(AppNetConfig.BASE_URL+"app_new.apk");
+                    //打开连接
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                    conn.setReadTimeout(5000);//读取超时
+                    conn.setConnectTimeout(5000);//连接超时
+                    conn.setRequestMethod("GET");//请求方式
+                    conn.connect();//连接网络
+
+                    if (conn.getResponseCode() == 200) {//连网成功
+                        //进度条的总长度
+                        progressDialog.setMax(conn.getContentLength());
+
+                        inputStream = conn.getInputStream();
+
+                        File path;
+                        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                            path = getExternalFilesDir("");
+                        } else {
+                            path = getFilesDir();
+                        }
+                        File file = new File(path, "update.apk");
+                        os = new FileOutputStream(file);
+
+                        byte[] bytes = new byte[1024];
+                        int len;
+                                                        /*
+                        * inputStream.read(bytes) 将数据装到bytes数组里
+                        * */
+                        while ((len = inputStream.read(bytes)) != -1) {
+                            progressDialog.incrementProgressBy(len);
+                            os.write(bytes, 0, len);
+                        }
+
+                        //下载成功了
+                        progressDialog.dismiss();
+                        //安装
+                        Intent intent = new Intent("android.intent.action.INSTALL_PACKAGE");
+                        intent.setData(Uri.parse("file:" + file.getAbsolutePath()));
+                        startActivity(intent);
+                    } else {//连网失败
+                        Toast.makeText(WelcomeActivity.this, "没网你看着办", Toast.LENGTH_SHORT).show();
+                        toMain();
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (os != null) {
+                        try {
+                            os.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (inputStream != null) {
+                        try {
+                            inputStream.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+            }
+        });
+
+    }
+
+    public void toMain() {
+        if (isLogin()) {
+            //登录过进入主界面
+            startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+            finish();
+        } else {
+            //没有登录过进入登录界面
+            startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            finish();
+        }
+    }
+
+    private boolean isLogin() {
+        String name = getUser().getData().getName();
+        if (TextUtils.isEmpty(name)) {
+            return false;
+        }
+        return true;
+    }
+
+    //判断是否有网络
+    private boolean isOnLine() {
+        boolean connected = false;
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        if (networkInfo != null) {
+            connected = networkInfo.isConnected();
+        }
+        // return connected;
+        return true;
     }
 
     @Override
--- app/src/main/java/com/atguigu/p2p/more/MoreFragment.java
+++ app/src/main/java/com/atguigu/p2p/more/MoreFragment.java
@@ -23,6 +23,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import butterknife.InjectView;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 /**
  * Created by ${
@@ -141,6 +142,38 @@ public class MoreFragment extends BaseFragment {
                         .show();
             }
         });
+
+        //分享
+        tvMoreShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OnekeyShare oks = new OnekeyShare();
+                //关闭sso授权
+                oks.disableSSOWhenAuthorize();
+                // title标题，印象笔记、邮箱、信息、微信、人人网、QQ和QQ空间使用
+                oks.setTitle("标题");
+                // titleUrl是标题的网络链接，仅在Linked-in,QQ和QQ空间使用
+                oks.setTitleUrl("http://www.baidu.com");
+                // text是分享文本，所有平台都需要这个字段
+                oks.setText("我是卖菊男孩景园");
+                //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+                oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+                // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+                //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+                // url仅在微信（包括好友和朋友圈）中使用
+                oks.setUrl("http://www.baidu.com");
+                // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+                oks.setComment("卖菊啦");
+                // site是分享此内容的网站名称，仅在QQ空间使用
+                oks.setSite("ShareSDK");
+                // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+                oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+                oks.show(getActivity());
+            }
+        });
+
     }
 
     private void setSetting(boolean setting) {
